d4818c69cff08b1b9ac3e818490777b0dde948f6
gowtham <gowthamkvdl@gmail.com>
2025-10-29 19:14:26 +0530

    bff

--- client/src/components/DietPlanner.jsx
+++ client/src/components/DietPlanner.jsx
@@ -92,7 +92,7 @@ const DietPlanner = () => {
 
     // --- RENDER LOGIC ---
     return (
-        <div className="container mt-3 mt-md-5">
+        <div className="container mt-2 mt-md-5">
             <header className="text-center mb-5">
                 <h1 className="display-4 text-success pt-5 font-weight-bold">
                     Smart Budget Diet Planner
