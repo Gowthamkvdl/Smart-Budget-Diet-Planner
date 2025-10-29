954a473318cf9e09b8c938b9f927928d02c461fd
gowtham <gowthamkvdl@gmail.com>
2025-10-29 19:17:50 +0530

    jytyj

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,10 @@ import DietPlanner from './components/DietPlanner'
 import ParticalBG from './components/ParticalBG'
 
 function App() {
-
+  // Disable hover effects on touch devices to avoid sticky :hover
+  if ('ontouchstart' in window || navigator.maxTouchPoints > 0) {
+    document.documentElement.classList.add('no-hover');
+  }
   return (
     <div className=''>
       <ParticalBG />
